71c3da608185371dcf3221b2dd4d3a3ee8110f45
DeepoweRs <bedirhan_kurt_@outlook.com>
2025-01-01 17:13:28 +0100

    Fix

--- assembly-endgame/src/compontens/Header.jsx
+++ assembly-endgame/src/compontens/Header.jsx
@@ -1,8 +1,10 @@
+import {languages} from "../data/languages.js";
+
 export default function Header() {
     return (
         <header className={"flex flex-col items-center justify-center gap-4 text-center"}>
             <h1 className={"text-3xl font-semibold text-text"}>Assembly: Endgame</h1>
-            <p className={"text-lg text-gray-500"}>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            <p className={"text-lg text-gray-500"}>Guess the word in under {languages.length - 1} attempts to keep the programming world safe from Assembly!</p>
         </header>
     )
 }
